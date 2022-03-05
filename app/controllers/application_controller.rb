d2f2e42066519a7a048beef3e0f46782ee9dc48a
shimi1991 <yuu.9134@gmail.com>
2022-03-05 12:41:14 +0900

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:belongs])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:position])
+  end
+end
